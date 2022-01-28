97b0e9db41cfcc66340a7e07da83d303da2e44c4
Jaeriah Tay <jaeriahpasseri@gmail.com>
2022-01-27 16:34:24 -0800

    Add about single content type

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -7,18 +7,9 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::category.category', ({ strapi }) =>  ({
-  // Replace category findOne action with slug instead of default id
+  // Add custom action with slug instead of default id
   async findOneBySlug(ctx) {
     const { slug } = ctx.params;
-    const { query } = ctx;
-
-    // if (!query.filters) {
-    //   query.filters = {};
-    // }
-
-    // query.fitlers.slug = {
-    //   $eq: slug,
-    // }
 
     const entity = await strapi.service('api::category.category').findOne({ slug });
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
--- src/api/category/routes/custom-category.js
+++ src/api/category/routes/custom-category.js
@@ -4,7 +4,7 @@ module.exports = {
   routes: [
     { // Path defined with a slug parameter
       method: 'GET',
-      path: '/categories/:slug',
+      path: '/categories/slug/:slug',
       handler: 'category.findOneBySlug',
       config: {
         policies: [],
